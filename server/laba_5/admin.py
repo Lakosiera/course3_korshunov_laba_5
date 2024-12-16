7feeab2c2c3ea4b243bc74f1d3aa0b1fb1649cc7
Lakosiera <lakosiera@mail.ru>
2024-12-17 03:24:34 +0500

    альбомы

--- server/laba_5/admin.py
+++ server/laba_5/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Album
+
+
+# класс обвертка над моделью 'Album' для работы в админке
+class AlbumAdmin(admin.ModelAdmin):
+    # отображаем поля обьекта в админке
+    list_display = ('id', 'title', 'artist', 'relesased_at', 'length')
+
+
+# регистрируем классы моделей бля работы с ними в админке
+admin.site.register(Album, AlbumAdmin)
--- server/laba_5/models.py
+++ server/laba_5/models.py
@@ -1,2 +1,33 @@
 from django.db import models
 
+# классы моделей (абстрактны структуры данных бля работы с базой данных)
+
+
+# класс бля альбома
+class Album(models.Model):
+    # поле индекса нужно для сохранения прогресса
+    id = models.AutoField(
+        primary_key=True,
+        help_text="поле индекса записи",
+    )
+    # название альбома
+    title = models.CharField(
+        max_length=50,  # максимальная длина текста
+        help_text="Название альбома",
+    )
+    # дата выпуска
+    relesased_at = models.DateTimeField()
+    # количесво трэков
+    length = models.IntegerField()
+    # артист
+    artist = models.CharField(
+        max_length=50,  # максимальная длина текста
+    )
+    # жанр
+    genre = models.CharField(
+        max_length=50,  # максимальная длина текста
+    )
+    # тип альбома (Studio / Live / Solo)
+    type = models.CharField(
+        max_length=50,  # максимальная длина текста
+    )
